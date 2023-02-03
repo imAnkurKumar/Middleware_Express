077a1e6a8a787f0ecfae54f3850dee080ef50b20
imAnkurKumar <imankur.ak@gmail.com>
2023-02-03 18:13:02 +0530

    middleware1

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+
+app.use((req, res, next) => {
+  console.log("inside middleware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("inside middleware2");
+  res.send("<h1>Hello from express</h1>")
+});
+
+http.createServer(app);
+app.listen(4000);
