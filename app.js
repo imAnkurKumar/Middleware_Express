ae0b5e363d4fa872828ffc13e6aee7f263f44a6f
imAnkurKumar <imankur.ak@gmail.com>
2023-03-20 08:21:46 +0530

    adding css

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -6,12 +7,13 @@ const adminRouters = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/admin", adminRouters);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("404 Not Found");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,11 @@
+const path = require("path");
 const express = require("express");
 const router = express.Router();
 
+const rootDir = require("../util/path");
+
 router.get("/add-product", (req, res, next) => {
-  res.send(
-    '<form action="/admin/add-product" method="POST"> <input type="text" name="title"> <input type="text" name="size"> <button type ="submit">Add Product</button></form>'
-  );
+  res.sendFile(path.join(rootDir, "views", "addProduct.html"));
 });
 
 router.post("/add-product", (req, res, next) => {
